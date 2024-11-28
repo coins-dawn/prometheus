405dabaf372a6e70e66a668d8492a3d02f3e1365
igarashi339 <igarashi339@gmail.com>
2024-11-28 22:24:59 +0900

    ポートおよびイメージを変更

--- script/convert.sh
+++ script/convert.sh
@@ -3,6 +3,6 @@
 docker run --rm \
     -e JAVA_TOOL_OPTIONS='-Xmx8g' \
     -v "$(pwd)/network/input:/var/opentripplanner" \
-    docker.io/opentripplanner/opentripplanner:latest --build --save
+    docker.io/opentripplanner/opentripplanner:2.7.0_2024-11-26T16-34 --build --save
 
 mv $(pwd)/network/input/graph.obj network
