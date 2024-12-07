a83be48d67cf618d1124ed33fcf65edff09a6cd1
igarashi339 <igarashi339@gmail.com>
2024-12-07 15:46:39 +0900

    公共交通のリクエストが失敗することのテストを追加

---
+++ test/test_ptrans.py
@@ -0,0 +1,20 @@
+import requests
+import json
+
+url = "http://localhost:3000/route/ptrans"
+headers = {"Content-Type": "application/json"}
+
+
+def test_route_ptrans():
+    payload = {
+        "org_coord": {"lat": 34.17343, "lon": 134.6264},
+        "dst_coord": {"lat": 34.18884, "lon": 134.5953},
+        "start_time": "2024-12-07T15:10:00",
+    }
+    
+    response = requests.post(url, headers=headers, data=json.dumps(payload))
+    assert response.status_code == 400
+    # assert response.status_code == 200
+    # response_data = response.json()
+    # assert response_data["status"] == "OK"
+    # result = response_data["result"]
