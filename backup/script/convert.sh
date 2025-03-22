08c07eb8a370d530dea020b00379dbf32252bc96
igarashi339 <igarashi339@gmail.com>
2025-03-22 13:59:02 +0900

    不要ファイルを削除

--- backup/script/convert.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker run --rm \
-    -e JAVA_TOOL_OPTIONS='-Xmx8g' \
-    -v "$(pwd)/network/input:/var/opentripplanner" \
-    docker.io/opentripplanner/opentripplanner:2.7.0_2024-11-26T16-34 --build --save
-
-mv $(pwd)/network/input/graph.obj network
