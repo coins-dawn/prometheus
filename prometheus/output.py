7bc40db0174275c02ab0b67c52890b84f16067a8
igarashi339 <igarashi339@gmail.com>
2025-03-23 15:21:30 +0900

    経路の描画ファイルを出力する

--- prometheus/output.py
+++ prometheus/output.py
@@ -1,7 +1,6 @@
 from dataclasses import dataclass
 
 from stop import Stop
-from coord import Coord
 
 
 @dataclass
---
+++ prometheus/visualize.py
@@ -0,0 +1,24 @@
+from output import SearchOutout
+import simplekml
+import polyline
+
+
+def generate_kml(search_output: SearchOutout, output_path="route.kml"):
+    kml = simplekml.Kml()
+
+    # ピンの作成（Stops）
+    for output_stop in search_output.route.stops:
+        stop = output_stop.stop
+        kml.newpoint(
+            name=stop.name, coords=[(stop.coord.lon, stop.coord.lat)]  # (lon, lat)
+        )
+
+    # 線の作成（Sections）
+    for section in search_output.route.sections:
+        coords = polyline.decode(section.shape)  # [(lat, lon), ...]
+        line = kml.newlinestring(coords=[(lon, lat) for lat, lon in coords])
+        line.style.linestyle.color = simplekml.Color.lightgreen  # 緑
+        line.style.linestyle.width = 4  # 太め
+
+    # 書き出し
+    kml.save(output_path)
