c1a4a5a48ee97817f5a36fa79f51015e37d54058
igarashi339 <igarashi339@gmail.com>
2025-03-23 15:11:38 +0900

    不要なコメントを削除

--- prometheus/app.py
+++ prometheus/app.py
@@ -16,12 +16,10 @@ def generate_route():
     except Exception as e:
         return jsonify({"status": "NG", "message": str(e)}), 400
 
-    search_output = searcher.search(search_input)
-
-    # try:
-    #     search_output = searcher.search(search_input)
-    # except Exception as e:
-    #     return jsonify({"status": "NG", "message": str(e)}), 500
+    try:
+        search_output = searcher.search(search_input)
+    except Exception as e:
+        return jsonify({"status": "NG", "message": str(e)}), 500
 
     return (
         jsonify(
@@ -32,27 +30,6 @@ def generate_route():
         ),
         200,
     )
-
-    # if "stops" not in data or not isinstance(data["stops"], list):
-    #     return jsonify({"error": "Invalid input format"}), 400
-
-    # try:
-    #     # coord を取り出して lat, lon に変換
-    #     coords = []
-    #     for stop in data["stops"]:
-    #         lat_str, lon_str = stop["coord"].split(",")
-    #         coords.append((float(lat_str.strip()), float(lon_str.strip())))
-
-    #     # ノード列を取得して巡回ルートを形成
-    #     node_sequence = [searcher.find_nearest_node(lat, lon) for lat, lon in coords]
-    #     node_sequence.append(node_sequence[0])  # 最後にスタート地点に戻る
-
-    #     route = searcher.find_route_through_nodes(node_sequence)
-
-    #     # searcher.export_kml(route, node_sequence)
-    #     return jsonify({"status": "OK"}), 200
-    # except Exception as e:
-    #     return jsonify({"error": str(e)}), 500
 
 
 if __name__ == "__main__":
