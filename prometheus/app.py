adc6e9b29a8999d64487897da3580987a840f4d9
igarashi339 <igarashi339@gmail.com>
2025-05-03 18:42:57 +0900

    時刻表を出力する

--- prometheus/app.py
+++ prometheus/app.py
@@ -37,3 +37,4 @@ def generate_route():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
+
--- prometheus/input.py
+++ prometheus/input.py
@@ -5,3 +5,4 @@ from prometheus.stop import Stop
 class SearchInput(BaseModel):
     route_name: str = Field(alias="route-name")
     stops: list[Stop]
+    start_time: str = Field(alias="start-time")
