30a35bb1b973d85011d85c0e65e48dbbaa0cbeea
igarashi339 <igarashi339@gmail.com>
2025-06-01 11:21:50 +0900

    shapeの型式を変更&start-timeを追加

--- prometheus/ptrans/ptrans_input.py
+++ prometheus/ptrans/ptrans_input.py
@@ -6,4 +6,5 @@ from prometheus.car.car_output import CarSearchOutput
 class PtransSearchInput(BaseModel):
     start: Coord
     goal: Coord
+    start_time: str = Field(alias="start-time")
     car_output: CarSearchOutput = Field(alias="car-output")
