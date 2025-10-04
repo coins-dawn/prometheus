f2d21766e0274cf5b4efc943f37b0a7471e3fa03
igarashi339 <igarashi339@gmail.com>
2025-10-05 00:44:18 +0900

    エンドポイントを修正、スポット一覧取得APIを追加

--- prometheus/area/area_searcher.py
+++ prometheus/area/area_searcher.py
@@ -1,4 +1,3 @@
-import json
 from shapely.geometry import shape
 from shapely.geometry.polygon import Polygon
 
@@ -9,19 +8,13 @@ from prometheus.area.area_search_output import (
     ReachableArea,
     Spot,
 )
+from prometheus.data_loader import (
+    load_spot_list,
+    load_geojson,
+    SPOT_LIST_FILE_PATH,
+)
 from prometheus.area.spot_type import SpotType
 from prometheus.coord import Coord
-
-
-SPOT_LIST_FILE_PATH = "data/area/toyama_spot_list.json"
-
-
-def load_spot_list(file_path: str):
-    """
-    スポット一覧をロードする。
-    """
-    with open(file_path, "r", encoding="utf-8") as f:
-        return json.load(f)
 
 
 def calc_target_time_limit(original_max_minute: int) -> int:
@@ -31,15 +24,6 @@ def calc_target_time_limit(original_max_minute: int) -> int:
     assert original_max_minute > 0
     assert original_max_minute <= 120
     return (original_max_minute // 10) * 10
-
-
-def load_geojson(id_str: str, max_minute: int):
-    """
-    指定されたIDと最大時間に対応するGeoJSONをロードする。
-    """
-    file_path = f"data/area/geojson/{id_str}_{max_minute}.geojson"
-    with open(file_path, "r", encoding="utf-8") as f:
-        return json.load(f)
 
 
 def exec_single_spot_type(
@@ -71,7 +55,7 @@ def exec_area_search(search_input: AreaSearchInput) -> AreaSearchOutput:
     到達圏検索を実行する。
     """
     target_max_limit = calc_target_time_limit(search_input.max_minute)
-    all_spot_list = load_spot_list(SPOT_LIST_FILE_PATH)
+    all_spot_list = load_spot_list()
     result_dict: dict[SpotType, AreaSearchResult] = {}
     for spot_type in search_input.target_spots:
         area_search_result = exec_single_spot_type(
